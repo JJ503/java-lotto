d8d474a0a3c5df1fd2468e500f3e5a9c6f3dc951
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 05:04:01 +0900

    feat(InputValidation): 당첨 번호 입력 값 검증 기능 추가
    docs: 당첨 번호 입력 값 검증 및 관련 기능 완료 체크

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -3,7 +3,11 @@ package lotto.validation;
 import lotto.constants.ExceptionMessage;
 import lotto.constants.LottoConstants;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputValidation {
+    private static final String COMMA = ",";
 
     public static Integer validateAmount(String amount) {
         isExist(amount);
@@ -11,6 +15,14 @@ public class InputValidation {
         isZero(amountNumber);
         isAmountUnity(amountNumber);
         return amountNumber;
+    }
+
+    public static List<Integer> validateWinningNumbers(String numbers) {
+        isExist(numbers);
+        List<Integer> winningNumbers = toIntegerList(numbers);
+        isSix(winningNumbers.size());
+
+        return winningNumbers;
     }
 
     private static void isExist(String inputValue) {
@@ -38,6 +50,28 @@ public class InputValidation {
     private static void isAmountUnity(Integer inputValue) {
         if (inputValue % LottoConstants.AMOUNT_UNIT != LottoConstants.ZERO) {
             ExceptionMessage.INPUT_AMOUNT_UNITY.throwException();
+        }
+    }
+
+    private static List<Integer> toIntegerList(String inputValue) {
+        try {
+            return toList(inputValue).stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException exception) {
+            ExceptionMessage.NOT_INPUT_NUMBER.throwException();
+        }
+
+        return null;
+    }
+
+    private static List<String> toList(String inputValue) {
+        return List.of(inputValue.split(COMMA));
+    }
+
+    private static void isSix(int size) {
+        if (size != LottoConstants.LOTTO_COUNT) {
+            ExceptionMessage.LOTTO_IS_SIX_LOTTO_NUMBER.throwException();
         }
     }
 }
