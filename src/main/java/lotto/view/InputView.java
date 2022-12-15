93311365243b7f6c1abe7669bb7fdc16d3045c2c
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 05:04:33 +0900

    feat(InputView): 당첨 번호 검증 기능 연결
    docs: 당첨 번호 검증 연결 완료 체크

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,11 +3,19 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.validation.InputValidation;
 
+import java.util.List;
+
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public int readPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         return InputValidation.validateAmount(Console.readLine());
+    }
+
+    public List<Integer> readWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS);
+        return InputValidation.validateWinningNumbers(Console.readLine());
     }
 }
