372f46e0db345f6efb484525130f6c797a498e71
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 03:46:42 +0900

    feat(InputView): 구매금액 검증 연결 기능 추가
    docs: 구매금액 검증 연결 완료 체크

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,13 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validation.InputValidation;
 
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
 
-    public String readPurchaseAmount() {
+    public int readPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
-        return Console.readLine();
+        return InputValidation.validateAmount(Console.readLine());
     }
 }
