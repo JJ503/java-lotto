910b59fd8ce017b46015d4bde43a3173eae1abd0
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 05:54:43 +0900

    feat(Rank): 정답 개수와 보너스 번호에 따른 순위 정보 가져오기 기능 추가

--- src/main/java/lotto/constants/Rank.java
+++ src/main/java/lotto/constants/Rank.java
@@ -1,5 +1,10 @@
 package lotto.constants;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Rank {
     FIRST_PRIZE(2_000_000_000, 6, false),
     SECOND_PRIZE(30_000_000, 5, true),
@@ -7,6 +12,9 @@ public enum Rank {
     FORTH_PRIZE(50_000, 4, false),
     FIFTH_PRIZE(5_000, 3, false),
     NONE(0, 0, false);
+
+    private static final Map<Integer, Rank> RANK_BY_MATCH_COUNT = Stream.of(values())
+            .collect(Collectors.toMap(Rank::getMatchCount, Function.identity()));
 
     private final int prizeMoney;
     private final int matchCount;
@@ -16,5 +24,25 @@ public enum Rank {
         this.prizeMoney = prizeMoney;
         this.matchCount = matchCount;
         this.bonus = bonus;
+    }
+
+    private int getMatchCount() {
+        return getMatchCount();
+    }
+
+    public static Rank getRank(int matchCount, boolean bonus) {
+        if (matchCount == SECOND_PRIZE.matchCount) {
+            return isSecondOrThirdPrize(bonus);
+        }
+
+        return RANK_BY_MATCH_COUNT.get(matchCount);
+    }
+
+    private static Rank isSecondOrThirdPrize(boolean bonus) {
+        if (bonus) {
+            return SECOND_PRIZE;
+        }
+
+        return THIRD_PRIZE;
     }
 }
