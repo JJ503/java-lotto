80ecca7f8b3cc4b3c41b94b6841beab59791974f
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 03:47:33 +0900

    feat(LottoConstants): 로또 게임에 반복적으로 사용될 상수 모음 클래스 생성

---
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class LottoConstants {
+    public static final int LOTTO_START_NUMBER = 1;
+    public static final int LOTTO_END_NUMBER = 45;
+    public static final int LOTTO_COUNT = 6;
+    public static final int AMOUNT_UNIT = 1000;
+    public static final int ZERO = 0;
+}
