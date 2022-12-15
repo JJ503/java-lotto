05ef0d4bea84f13595f0a0b1648edd5870443473
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 03:45:52 +0900

    feat(InputValidation): 로또 구매 금액 입력 값 검증 기능 추가
    docs: 로또 구매 금액 입력 값 검증 및 관련 기능 완료 체크

---
+++ src/main/java/lotto/validation/InputValidation.java
@@ -0,0 +1,43 @@
+package lotto.validation;
+
+import lotto.constants.ExceptionMessage;
+import lotto.constants.LottoConstants;
+
+public class InputValidation {
+
+    public static Integer validateAmount(String amount) {
+        isExist(amount);
+        Integer amountNumber = toNumber(amount);
+        isZero(amountNumber);
+        isAmountUnity(amountNumber);
+        return amountNumber;
+    }
+
+    private static void isExist(String inputValue) {
+        if (inputValue.isEmpty()) {
+            ExceptionMessage.NONE_INPUT.throwException();
+        }
+    }
+
+    private static Integer toNumber(String inputValue) {
+        try {
+            return Integer.parseInt(inputValue);
+        } catch (NumberFormatException numberFormatException) {
+            ExceptionMessage.NOT_INPUT_NUMBER.throwException();
+        }
+
+        return null;
+    }
+
+    private static void isZero(Integer inputValue) {
+        if (inputValue == LottoConstants.ZERO) {
+            ExceptionMessage.INPUT_MORE_THAN_ZERO.throwException();
+        }
+    }
+
+    private static void isAmountUnity(Integer inputValue) {
+        if (inputValue / LottoConstants.AMOUNT_UNIT != LottoConstants.ZERO) {
+            ExceptionMessage.INPUT_AMOUNT_UNITY.throwException();
+        }
+    }
+}
