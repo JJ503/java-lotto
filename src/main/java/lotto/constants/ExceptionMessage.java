b52316486d9ff177552c4b19d36bf1a16b8565eb
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 03:48:22 +0900

    feat(ExceptionMessage): 예외 메시지 상수 추가
    - 숫자를 입력하지 않은 경우
    - 0 이하의 값을 입력한 경우
    - 1000 단위가 아닌 값은 입력한 경우

--- src/main/java/lotto/constants/ExceptionMessage.java
+++ src/main/java/lotto/constants/ExceptionMessage.java
@@ -1,7 +1,10 @@
 package lotto.constants;
 
 public enum ExceptionMessage {
-    NONE_INPUT("값을 입력해 주시길 바랍니다.");
+    NONE_INPUT("값을 입력해 주셔야 합니다."),
+    NOT_INPUT_NUMBER("숫자를 입력해 주셔야 합니다."),
+    INPUT_MORE_THAN_ZERO("0을 초과한 값을 입력해 주셔야 합니다."),
+    INPUT_AMOUNT_UNITY("천 단위의 숫자를 입력해 주셔야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
     private final String exceptionMessage;
