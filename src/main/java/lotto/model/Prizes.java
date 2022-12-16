16f390a4642dbfacb66f5dc910ee58d7ff2317c7
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 18:33:29 +0900

    fix(Prize): prize 관련 맵에 None 랭킹은 들어가지 않도록 수정 및 초기화 수정

--- src/main/java/lotto/model/Prizes.java
+++ src/main/java/lotto/model/Prizes.java
@@ -6,10 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Prizes {
-    private final Map<Rank, Integer> prizes = new HashMap<>();
+    private final Map<Rank, Integer> prizes;
     private int totalPrizeMoney = 0;
 
     public Prizes() {
+        prizes = new HashMap<>();
         prizes.put(Rank.FIRST_PRIZE, 0);
         prizes.put(Rank.SECOND_PRIZE, 0);
         prizes.put(Rank.THIRD_PRIZE, 0);
@@ -19,12 +20,14 @@ public class Prizes {
 
     public void addPrize(Prize prize) {
         Rank rank = prize.getRank();
-        prizes.put(rank, prizes.get(rank) + 1);
-        totalPrizeMoney += rank.getPrizeMoney();
+        if (!rank.equals(Rank.NONE)) {
+            prizes.put(rank, prizes.get(rank) + 1);
+            totalPrizeMoney += rank.getPrizeMoney();
+        }
     }
 
-    public Map<Rank, Integer> getPrizes() {
-        return prizes;
+    public int getPrizes(Rank rank) {
+        return prizes.get(rank);
     }
 
     public int getTotalPrizeMoney() {
