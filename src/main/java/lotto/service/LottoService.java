b70338f3b9429d0ba786974a17bbdfcad23f16c9
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 18:51:01 +0900

    fix(LottoService): 둘째자리에서 반올림되지 않는 문제 해결

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -39,6 +39,6 @@ public class LottoService {
     }
 
     public double roundSecondDigit(double value) {
-        return Math.round(value * 100) / 100;
+        return Math.round(value * 10) / 10.0;
     }
 }
