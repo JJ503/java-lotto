7d2cb13bc0bf539107bf57689c4a382e1fb60edf
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 18:38:39 +0900

    feat(OutputView):  당첨 통계 출력하기 기능 추가
    docs:  당첨 통계 출력하기 및 관련 기능 완료 체크

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,36 @@
 package lotto.view;
 
+import lotto.constants.Rank;
 import lotto.model.Lotto;
 import lotto.model.Lottos;
+import lotto.model.Prizes;
 
 public class OutputView {
     private static final String LOTTO_TICKET_COUNT_FORMAT = "\n%d개를 구매했습니다.\n";
+    private static final String LOTTO_RESULT_FORMAT = "\n당첨 통계\n---";
+    private static final String LOTT_RATE_OF_RETURN = "총 수익률은 %,.1f%%입니다.\n";
 
     public void printLottoTickets(Lottos lottos) {
         System.out.printf(LOTTO_TICKET_COUNT_FORMAT, lottos.getLottos().size());
         for (Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto.getLotto());
         }
+    }
+
+    public void printLottoResult(Prizes prizes) {
+        System.out.println(LOTTO_RESULT_FORMAT);
+        for (Rank rank : Rank.values()) {
+            printRankResult(prizes, rank);
+        }
+    }
+
+    private void printRankResult(Prizes prizes, Rank rank) {
+        if (rank != Rank.NONE) {
+            System.out.println(rank.getRankMessage(prizes.getPrizes(rank)));
+        }
+    }
+
+    public void printRateOfReturn(double rateOfReturn) {
+        System.out.printf(LOTT_RATE_OF_RETURN, rateOfReturn);
     }
 }
