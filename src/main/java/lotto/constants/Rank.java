d5be144a2426716e7059acfc2ef968a0d453bc64
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 18:35:44 +0900

    fix(Rank): Second와 Third의 키기 같은 문제 해결

--- src/main/java/lotto/constants/Rank.java
+++ src/main/java/lotto/constants/Rank.java
@@ -1,20 +1,21 @@
 package lotto.constants;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public enum Rank {
-    FIRST_PRIZE(2_000_000_000, 6),
-    SECOND_PRIZE(30_000_000, 5),
-    THIRD_PRIZE(1_500_000, 5),
-    FORTH_PRIZE(50_000, 4),
     FIFTH_PRIZE(5_000, 3),
+    FORTH_PRIZE(50_000, 4),
+    THIRD_PRIZE(1_500_000, 5),
+    SECOND_PRIZE(30_000_000, 5),
+    FIRST_PRIZE(2_000_000_000, 6),
     NONE(0, 0);
 
-    private static final Map<Integer, Rank> RANK_BY_MATCH_COUNT = Stream.of(values())
-            .collect(Collectors.toMap(Rank::getMatchCount, Function.identity()));
+    private static final Map<Integer, Rank> RANK_BY_MATCH_COUNT = new HashMap<>() {{
+        put(6, Rank.FIRST_PRIZE);
+        put(4, Rank.FORTH_PRIZE);
+        put(3, Rank.FIFTH_PRIZE);
+    }};
     private static final String SECOND_PRIZE_MESSAGE_FORMAT = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
     private static final String MESSAGE_FORMAT = "%d개 일치 (%,d원) - %d개";
 
@@ -30,10 +31,6 @@ public enum Rank {
         return prizeMoney;
     }
 
-    private int getMatchCount() {
-        return matchCount;
-    }
-
     public String getRankMessage(int count) {
         if (this.equals(Rank.SECOND_PRIZE)) {
             return String.format(SECOND_PRIZE_MESSAGE_FORMAT, matchCount, prizeMoney, count);
@@ -47,7 +44,7 @@ public enum Rank {
             return isSecondOrThirdPrize(bonus);
         }
 
-        return RANK_BY_MATCH_COUNT.get(matchCount);
+        return isOtherPrize(matchCount);
     }
 
     private static Rank isSecondOrThirdPrize(boolean bonus) {
@@ -56,5 +53,13 @@ public enum Rank {
         }
 
         return THIRD_PRIZE;
+    }
+
+    private static Rank isOtherPrize(int matchCount) {
+        if (RANK_BY_MATCH_COUNT.containsKey(matchCount)) {
+            return RANK_BY_MATCH_COUNT.get(matchCount);
+        }
+
+        return Rank.NONE;
     }
 }
